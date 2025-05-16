3e294e0edd9734b4045c552b6cd7d70c744a02cb
Dinuvi9170 <dinuvitharushika@gmail.com>
2025-05-16 12:44:50 +0530

    fix(SupplierCard): adjust image display properties and update address styling

--- frontend/src/components/supplierCard/supplierCard.jsx
+++ frontend/src/components/supplierCard/supplierCard.jsx
@@ -6,15 +6,15 @@ const SupplierCard = ({ supplier }) => {
     console.log("Supplier data in card:", supplier);
     return (
         <div className="supplierCard">
-<Link to={`/suppliers/${supplier._id}`} className="supplierCard__link">
+            <Link to={`/suppliers/${supplier._id}`} className="supplierCard__link">
             <div className="supplierCard-container">
                 <div className="supplierCard__picture">
                     <img src={supplier.cover} alt={supplier.cover} className="supplierCard__image" />
                 </div>
                 <div className="supplierCard__details">
                     <h2 className="shop__name">{supplier.ShopName}</h2>
-                    <p className="shop__address">{supplier.address}</p>
                     <p className="shop__title">{supplier.title}</p>
+                    <p className="shop__address">{supplier.address}</p>
                     <div className="supplierCard-user">
                         <img src={supplier.user_img} alt={supplier.username} className="gigCard__user-image" />
                         <p className='text2'>Created by</p>
